51e95f64197c38dfed7af92f618a465fb1ac822f
Martin Boháč <bohacc@seznam.cz>
2017-11-01 08:23:16 +0100

    recaptcha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,31 +29,6 @@ export class AppComponent implements OnInit {
     this.addScript();
   }
 
-  registerReCaptchaCallback() {
-    window.reCaptchaLoad = () => {
-      const config = {
-        ...this.config,
-        'sitekey': this.key,
-        'callback': this.onSuccess.bind(this),
-        'expired-callback': this.onExpired.bind(this)
-      };
-      this.widgetId = this.render(this.element.nativeElement, config);
-    };
-  }
-
-  private render( element: HTMLElement, config ): number {
-    return grecaptcha.render(element, config);
-  }
-
-  private addScript() {
-    let script = document.createElement('script');
-    const lang = this.lang ? '&hl=' + this.lang : '';
-    script.src = `https://www.google.com/recaptcha/api.js?onload=reCaptchaLoad&render=explicit${lang}`;
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-  }
-
   public addTeam() {
     this.http.post('/team', {})
     .subscribe(
