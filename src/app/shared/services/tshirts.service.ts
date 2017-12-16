45b4ea726dfad83be59b650e1e31ef15b1f2ca23
Martin Boháč <bohacc@seznam.cz>
2017-12-16 01:34:38 +0100

    registration, images

--- src/app/shared/services/tshirts.service.ts
+++ src/app/shared/services/tshirts.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../state/app.state';
 import { LoadTShirtsAction } from '../../modules/teams/state/actions/tshirts.action';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { TShirt } from '../../modules/teams/models/TShirt';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
 
 @Injectable()
-export class TshirtsService {
+export class TshirtsService implements OnDestroy {
   private itemsRef: AngularFireList<{tshirts: TShirt}[]>;
+  private unsubscribe: Subject<any> = new Subject();
 
   constructor(
     private store: Store<AppState>,
@@ -16,11 +19,17 @@ export class TshirtsService {
     this.itemsRef = this.db.list('tshirts');
   }
 
+  ngOnDestroy() {
+    this.unsubscribe.next(null);
+    this.unsubscribe.complete();
+  }
+
   public loadTShirts() {
     this.itemsRef.snapshotChanges()
       .map(changes => {
         return changes.map(c => ({...c.payload.val()}));
       })
+      .takeUntil(this.unsubscribe)
       .subscribe(
         (tshirts: TShirt[]) => {
           console.log('TSHIRTS');
