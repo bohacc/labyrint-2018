fcd6188a4c46be1fba405bf57a80b2be1c8a78b7
Martin Boháč <bohacc@seznam.cz>
2017-12-21 23:53:30 +0100

    registration

--- src/app/modules/teams/teams.routes.ts
+++ src/app/modules/teams/teams.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TeamsComponent } from './components/teams/teams.component';
 import { TeamRegistrationComponent } from './components/team-registration/team-registration.component';
+import { RegistrationSuccessComponent } from './components/registration-success/registration-success.component';
 
 export const routes: Routes = [
   { path: 'list', component: TeamsComponent },
-  { path: 'registration', component: TeamRegistrationComponent }
+  { path: 'registration', component: TeamRegistrationComponent },
+  { path: 'registration-success', component: RegistrationSuccessComponent }
 ];
