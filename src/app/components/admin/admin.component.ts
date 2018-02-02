dcf0567912882492f05295a728fed251c42da255
Martin Boháč <bohacc@seznam.cz>
2018-02-02 21:11:43 +0100

    edit registration

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -28,6 +28,9 @@ export class AdminComponent implements OnInit, OnDestroy {
   public tshirtsCount: number;
   public SummaryForPay: number;
   public SummaryPay: number;
+  public payAmountAccommodations: number;
+  public payAmountTshirts: number;
+  public paySentAmount: number;
   public teams: TeamDto[];
   public config: ConfigDbDto;
   private unsubscribe: Subject<any> = new Subject();
@@ -58,15 +61,54 @@ export class AdminComponent implements OnInit, OnDestroy {
     this.store.select(state => state)
       .subscribe((state) => {
         this.teams = state.teams.teams.list;
+        this.config = state.config.config;
         this.recalculate();
       });
   }
 
   private recalculate() {
     this.teamsCount = this.teams.length;
+
     this.teamsPayCount = this.teams.filter((team: TeamDto) => {
-      return !!team.paySent;
+      return team.paySent;
     }).length;
+
+    this.payAmountAccommodations = this.teams.map((team: TeamDto) => {
+      return this.accommodationsService.getAccommodation(team.accommodation).price;
+    }).reduce((a, b) => a + b, 0);
+
+    this.payAmountTshirts = this.teams.map((team: TeamDto) => {
+      let price = 0;
+      const tshirt1 = this.tshirtsService.getTshirts(team.player1.tshirt);
+      const tshirt2 = this.tshirtsService.getTshirts(team.player2.tshirt);
+      const tshirt3 = this.tshirtsService.getTshirts(team.player3.tshirt);
+      const tshirt4 = this.tshirtsService.getTshirts(team.player4.tshirt);
+      const tshirt5 = this.tshirtsService.getTshirts(team.player5.tshirt);
+      price += tshirt1 ? tshirt1.price : 0;
+      price += tshirt2 ? tshirt2.price : 0;
+      price += tshirt3 ? tshirt3.price : 0;
+      price += tshirt4 ? tshirt4.price : 0;
+      price += tshirt5 ? tshirt5.price : 0;
+      return price;
+    }).reduce((a, b) => a + b, 0);
+
+    this.paySentAmount = this.teams.map((team: TeamDto) => {
+      const accommodation = this.accommodationsService.getAccommodation(team.accommodation);
+      const tshirt1 = this.tshirtsService.getTshirts(team.player1.tshirt);
+      const tshirt2 = this.tshirtsService.getTshirts(team.player2.tshirt);
+      const tshirt3 = this.tshirtsService.getTshirts(team.player3.tshirt);
+      const tshirt4 = this.tshirtsService.getTshirts(team.player4.tshirt);
+      const tshirt5 = this.tshirtsService.getTshirts(team.player5.tshirt);
+      let tshirtPrice = 0;
+      tshirtPrice += tshirt1 ? tshirt1.price : 0;
+      tshirtPrice += tshirt2 ? tshirt2.price : 0;
+      tshirtPrice += tshirt3 ? tshirt3.price : 0;
+      tshirtPrice += tshirt4 ? tshirt4.price : 0;
+      tshirtPrice += tshirt5 ? tshirt5.price : 0;
+      const registrationPrice = this.config.registration_price || 0;
+      return (accommodation ? accommodation.price : 0) + tshirtPrice + registrationPrice;
+    }).reduce((a, b) => a + b, 0);
+
     this.peopleCount = this.teams.map((team: TeamDto) => {
       let count = 0;
       count += !!(team.player1.firstName + team.player1.lastName + team.player1.food) ? 1 : 0;
@@ -76,20 +118,41 @@ export class AdminComponent implements OnInit, OnDestroy {
       count += !!(team.player5.firstName + team.player5.lastName + team.player5.food) ? 1 : 0;
       return count;
     }).reduce((a, b) => a + b, 0);
+
     this.peopleAccommodationCount = this.teams.map((team: TeamDto) => {
-      return this.accommodationsService.getAccommodation(team.accommodation).price;
+      return this.accommodationsService.getAccommodation(team.accommodation).count;
     }).reduce((a, b) => a + b, 0);
+
     this.peopleBuildingCount = this.teams.map((team: TeamDto) => {
       const accommodation: AccommodationDto = this.accommodationsService.getAccommodation(team.accommodation);
       return AccommodationsTypesEnum.BUILDING === accommodation.type ? accommodation.count : 0;
     }).reduce((a, b) => a + b, 0);
+
     this.teamsBuildingCount = this.teams.map((team: TeamDto) => {
       const accommodation: AccommodationDto = this.accommodationsService.getAccommodation(team.accommodation);
       return AccommodationsTypesEnum.BUILDING === accommodation.type ? 1 : 0;
     }).reduce((a, b) => a + b, 0);
+
     this.peopleHutCount = this.teams.map((team: TeamDto) => {
       const accommodation: AccommodationDto = this.accommodationsService.getAccommodation(team.accommodation);
       return AccommodationsTypesEnum.HUT === accommodation.type ? accommodation.count : 0;
+    }).reduce((a, b) => a + b, 0);
+
+    this.teamsHutCount = this.teams.map((team: TeamDto) => {
+      const accommodation: AccommodationDto = this.accommodationsService.getAccommodation(team.accommodation);
+      return AccommodationsTypesEnum.HUT === accommodation.type ? 1 : 0;
+    }).reduce((a, b) => a + b, 0);
+
+    this.peopleFoodCount = this.peopleAccommodationCount;
+
+    this.tshirtsCount = this.teams.map((team: TeamDto) => {
+      let count = 0;
+      count += (team.player1.tshirt && this.tshirtsService.getTshirts(team.player1.tshirt).price > 0) ? 1 : 0;
+      count += (team.player2.tshirt && this.tshirtsService.getTshirts(team.player2.tshirt).price > 0) ? 1 : 0;
+      count += (team.player3.tshirt && this.tshirtsService.getTshirts(team.player3.tshirt).price > 0) ? 1 : 0;
+      count += (team.player4.tshirt && this.tshirtsService.getTshirts(team.player4.tshirt).price > 0) ? 1 : 0;
+      count += (team.player5.tshirt && this.tshirtsService.getTshirts(team.player5.tshirt).price > 0) ? 1 : 0;
+      return count;
     }).reduce((a, b) => a + b, 0);
   }
 }
--- src/app/modules/teams/services/tshirts.service.ts
+++ src/app/modules/teams/services/tshirts.service.ts
@@ -3,21 +3,27 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../../state/app.state';
 import { LoadTShirtsAction } from '../state/actions/tshirts.action';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { TShirt } from '../models/TShirtDto';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/takeUntil';
 import { TshirtDto } from '../../../shared/model/TshirtDto';
+import { State } from '../state/reducers/module.reducer';
 
 @Injectable()
 export class TshirtsService implements OnDestroy {
   private itemsRef: AngularFireList<{tshirts: TshirtDto}[]>;
   private unsubscribe: Subject<any> = new Subject();
+  private tshirts: TshirtDto[];
 
   constructor(
-    private store: Store<AppState>,
+    private store: Store<State>,
     private db: AngularFireDatabase
   ) {
     this.itemsRef = this.db.list('tshirts', ref => ref.orderByChild('sort_order') );
+    this.store.select(state => state)
+      .takeUntil(this.unsubscribe)
+      .subscribe((state) => {
+        this.tshirts = state.teams.tshirts.list;
+      });
   }
 
   ngOnDestroy() {
@@ -36,5 +42,11 @@ export class TshirtsService implements OnDestroy {
           this.store.dispatch(new LoadTShirtsAction(tshirts));
         }
       );
+  }
+
+  public getTshirts(code: string): TshirtDto {
+    return this.tshirts.filter((tshirt) => {
+      return tshirt.value === code;
+    })[0];
   }
 }
